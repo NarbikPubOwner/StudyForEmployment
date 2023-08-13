deaccb9151a20ba004286b5698f6925e92084221
NarbikPubOwner <113754405+NarbikPubOwner@users.noreply.github.com>
2023-08-13 18:18:31 +0900

    냉무

--- Algorithm/solution/20230808_09/NextPermutation.java
+++ Algorithm/solution/20230808_09/NextPermutation.java
@@ -1,40 +1,27 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class PermutationNPTest {
+public class combinationNPTest {
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		int R = sc.nextInt();
 		int[] input = new int[N];
-		int[] p = new int[N];
 		
 		for (int i = 0; i < input.length; i++) {
 			input[i] = sc.nextInt();
 		}
 		
-//		long start = System.nanoTime();
-//		Arrays.sort(input);//오름차순 정렬
-		
-		int cnt = 0;
-		while (++cnt<=R) p[N-cnt] = 1;
+		Arrays.sort(input);//오름차순 정렬
 		do {
-			//p배열을 이용한 조합 확인
-			for (int i = 0; i < N; i++) {
-				if (p[i]  == 0) continue;
-				System.out.print(input[i] + "\t");
-			}
-			System.out.println();
-		} while(np(p));
-//		long end = System.nanoTime();
-//		System.out.println((end-start)/1_000_000_000.0);
+			System.out.println(Arrays.toString(input));
+		} while(np(input));
 	}
 	
 	static boolean np(int[] p) { // p : 다음 순열을 원하는 기존 순열의 배열
 //		1. 꼭대기 찾기(꼭대기 앞뒤 모두 꼭대기보다 작은 수) → 꼭대기를 못 찾으면 리턴(ex4321)
-		int N = p.length;
-		int i = N-1;
+		int N = p.length;//뽑아올 배열이 매번 달라지므로
+		int i = N-1;//뒤부터 탐색
 		while (i>0 && p[i-1] >= p[i]) --i;
 		
 		if (i==0) {//맨앞자리수가 가장 크기 때문에 가장 큰 순열의 형태이다.
