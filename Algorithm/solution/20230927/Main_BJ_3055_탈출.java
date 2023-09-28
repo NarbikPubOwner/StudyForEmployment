4caf0dea04704c970aa52c2f1b6f61201e94a482
송무현 <113754405+NarbikPubOwner@users.noreply.github.com>
2023-09-28 09:08:13 +0900

    Update Main_BJ_3055_탈출.java

--- Algorithm/solution/20230927/Main_BJ_3055_탈출.java
+++ Algorithm/solution/20230927/Main_BJ_3055_탈출.java
@@ -2,22 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
- * 물 - 4방으로 퍼짐 -> 고슴도치, 비버 굴, 돌은 침식 불가 -> 사실상 .만 침식
- * 물의 개수가 정해져 있지 않다 -> 0개, 2개이상 고려해야함
+ * 물이 퍼진 곳에는 고슴도치가 갈 수 없다 -> 물을 먼저 퍼트리고 고슴도치의 bfs를 진행하면 된다
+ * 물이 1번 퍼지면 고슴도치가 1번 bfs를 진행할 수 있다 -> bfs 큐에 들어가는 순서는 길이 순서대로 들어가기 때문에(bfs의 특징) bfs의 원소 저장에 움직인 횟수를 추가하고, 그 움직인 횟수가 이전 기준값과 달라진다면 물이 이동해야 하는 시점이라고 판단 할 수 있다
+ * 물 또한 1번만 퍼트려야 하는데, 하나의 큐만 사용하면 구현하기 힘들기 때문에 2개의 큐를 사용하여 bfs의 진행과 저장을 분리해서 구현한다
+ * 1번 큐의 bfs를 진행하고 거기서 얻어진 다음 진행정보는 2번 큐에 저장한다 -> 이후엔 2번 큐의 bfs를 진행하고 이번엔 1번큐에 저장한다 -> 반복
  * 
- * 고슴도치 - 4방중 1택 - 물이 이동 예정인 곳으로 이동하면 안됨 -> 물 먼저 퍼트리기
- * 돌 - 물, 고슴도치 둘 다 이동 불가
- * 
- * 고슴도치가 비버의 굴로 이동할 수 있는 가장 빠른 시간 - bfs
- * 
- * 
+ * 주의사항 : 문제에 표기는 되어있지 않지만 물은 시작부터 없을수도 있고, 또 여러개가 있을 수도 있다. 때문에 모든 물의 위치를 시작할 때 큐에 넣어야 한다.
  */
-
 
 public class Main_BJ_3055_탈출 {
 	static int N;
@@ -36,7 +31,13 @@ public class Main_BJ_3055_탈출 {
 		int startBX = 0;
 		int startBY = 0;
 		
-		Queue<int[]> WQ = new ArrayDeque<>();
+		int prev = -1;//현재 진행중인 길이 
+		int swc = 0;//물 스위치
+		Queue<int[]> WQ = new ArrayDeque<>();//물 1번큐
+		Queue<int[]> WQ2 = new ArrayDeque<>();//물 2번큐
+		
+		boolean[][] visited = new boolean[N][M];//고슴도치 방문저장배열
+		Queue<int[]> SQ = new ArrayDeque<>();//고슴도치 큐
 		
 		for (int i = 0; i < N; i++) {
 			char[] tempCharArr = br.readLine().toCharArray();
@@ -52,23 +53,16 @@ public class Main_BJ_3055_탈출 {
 			}
 		}
 		
-		boolean[][] visited = new boolean[N][M];
-		Queue<int[]> SQ = new ArrayDeque<>();
-		
-		int swc = 0;
-		
-		Queue<int[]> WQ2 = new ArrayDeque<>();
-		
 		SQ.offer(new int[] {startBX, startBY, 0});
-		
-		int prev = -1;
+	
 		
 		while (!SQ.isEmpty()) {
 			int[] xyz = SQ.poll();
 			
-			if (xyz[2]-1 ==  prev) {
+			//물 bfs
+			if (xyz[2]-1 ==  prev) {//고슴도치의 진행 길이가 달라졌을 경우 물의 bfs를 진행한다
 				prev++;
-				//물 bfs1회
+				//물 큐 스위칭
 				Queue<int[]> targetWq = null;
 				Queue<int[]> nextWq = null;
 				if (swc == 0) {
@@ -104,13 +98,12 @@ public class Main_BJ_3055_탈출 {
 						SQ.offer(new int[] {nx,ny,xyz[2]+1});
 					}
 					else if(arr[nx][ny] == 'D') {
-						System.out.println(xyz[2]+1);
+						System.out.println(xyz[2]+1);//마지막 지점에 도달하면 정답 출력후 함수를 종료
 						System.exit(0);
 					}
 				}
 			}
 		}
-		System.out.println("KAKTUS");
-		
+		System.out.println("KAKTUS");//마지막 지점에 도착하지 못한 경우의 출력
 	}
 }
