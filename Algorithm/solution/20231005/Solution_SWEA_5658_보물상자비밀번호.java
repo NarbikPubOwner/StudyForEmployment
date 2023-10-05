71a6a97e4d3550350b6518b85d8d2deab9a05da1
송무현 <113754405+NarbikPubOwner@users.noreply.github.com>
2023-10-05 21:37:04 +0900

    Update Solution_SWEA_5658_보물상자비밀번호.java

--- Algorithm/solution/20231005/Solution_SWEA_5658_보물상자비밀번호.java
+++ Algorithm/solution/20231005/Solution_SWEA_5658_보물상자비밀번호.java
@@ -3,19 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
-/*
- * 반복
- * 4개로 자른다
- * 자른애들을 10진수로 변환해서 map에 넣는다
- * 회전 시킨다
- * 
- * 투포인터? 
- * 
- */
 public class Solution_SWEA_5658_보물상자비밀번호 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,23 +16,20 @@ public class Solution_SWEA_5658_보물상자비밀번호 {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			int N = Integer.parseInt(st.nextToken());
 			int K = Integer.parseInt(st.nextToken());
-			int subLength = N/4;
-			
-//			Set<Integer> set = new HashSet<>();
-			TreeSet<Integer> set = new TreeSet<>(Comparator.reverseOrder());
+			int subLength = N/4;//나눠질 문자열의 길이
+			TreeSet<Integer> set = new TreeSet<>(Comparator.reverseOrder());//중복이 없어야 하기 때문에 set 사용 + 정렬일 필요하기 때문에 treeSet + 내림차순 정렬을 위한 리버스오더
 			
 			StringBuilder sb = new StringBuilder(br.readLine());
-			for (int j = 0; j < 4; j++) {
-				int start = j*subLength;
-				set.add(Integer.parseInt(sb.substring(start, start + subLength),16));
-			}
-			for (int i = 1; i < subLength+1; i++) {
+			for (int i = 0; i <= subLength; i++) {//잘려질 문자열의 길이 = 회전 횟수이다
 				StringBuilder temp = new StringBuilder();
-				temp.append(sb.charAt(N-1)).append(sb.substring(0, N-1));
-				sb = temp;
+				temp.append(sb.charAt(N-1)).append(sb.substring(0, N-1));//문자열의 끝부분 + 0~N-2를 하면 회전된 문자열을 얻는다
+				sb = temp;//회전은 다음에도 반영되어야 하니 원본 문자열에 반입
+				/*
+				 * 문자열을 4개로 나누는 작업
+				 */
 				for (int j = 0; j < 4; j++) {
 					int start = j*subLength;
-					set.add(Integer.parseInt(sb.substring(start, start + subLength),16));
+					set.add(Integer.parseInt(sb.substring(start, start + subLength),16));//16진수 -> 10진수로 변환해 set에 삽입
 				}
 			}
 			
